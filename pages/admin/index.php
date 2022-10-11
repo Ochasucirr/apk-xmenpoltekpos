0b399c1e9f8c5df69f5700af4ab60d5a18ee9e3a
Sucirahmarosa <sucirahmarosa.03@gmail.com>
2022-10-11 12:18:28 +0700

    Ngubah cinta jadi benci

--- pages/admin/index.php
+++ pages/admin/index.php
@@ -57,7 +57,7 @@ if ($_SESSION['role'] != "Admin") {
     <div class="app-container app-theme-white body-tabs-shadow fixed-sidebar fixed-header">
         <div class="app-header header-shadow">
             <div class="app-header__logo">
-                <div class="logo-src"><a href="index.php">Anjing</a></div>
+                <div class="logo-src"><a href="index.php">Ocha Benci Aldo</a></div>
                 <div class="header__pane ml-auto">
                     <div>
                         <button type="button" class="hamburger close-sidebar-btn hamburger--elastic" data-class="closed-sidebar">
